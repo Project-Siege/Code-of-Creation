02c78dc867b538f749af14b2ff14e0e2abca4657
Nomadic Jester <133465352+Project-Siege@users.noreply.github.com>
2025-09-22 05:24:59 -0700

    hurt,hit,damage

--- scripts/state_machine/chopping_state.gd
+++ scripts/state_machine/chopping_state.gd
@@ -2,8 +2,12 @@ extends NodeState
 
 @export var player:Player
 @export var animated_sprite_2d: AnimatedSprite2D
+@export var hit_component_collision_shape: CollisionShape2D
 
 
+func _ready() -> void:
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2(0,0);
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -22,20 +26,28 @@ func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("slice_up")
 		animated_sprite_2d.flip_h = false
+		hit_component_collision_shape.position = Vector2(0, -36)
 	elif player.player_direction == Vector2.DOWN:
 		animated_sprite_2d.play("slice_down")
 		animated_sprite_2d.flip_h = false
+		hit_component_collision_shape.position = Vector2(0, 7)
 	elif player.player_direction == Vector2.LEFT:
 		animated_sprite_2d.play("slice_side")
 		animated_sprite_2d.flip_h = true
+		hit_component_collision_shape.position = Vector2(-23, -14)
 	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.flip_h = false
 		animated_sprite_2d.play("slice_side")
+		hit_component_collision_shape.position = Vector2(23, -14)
 		
 	else:
 		animated_sprite_2d.play("slice_down")
+		
+	hit_component_collision_shape.disabled = false
+	
 	
 
 
 func _on_exit() -> void:
 	animated_sprite_2d.stop()
+	hit_component_collision_shape.disabled = true
