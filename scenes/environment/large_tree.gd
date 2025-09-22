1d9bf451d1b64739e4f97b83e6b49b65fbb094fd
Nomadic Jester <133465352+Project-Siege@users.noreply.github.com>
2025-09-22 06:27:37 -0700

    tree shake

--- scenes/environment/large_tree.gd
+++ scenes/environment/large_tree.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
-@onready var tree_trunk: Sprite2D = $TreeTrunk
-@onready var tree_leave: Sprite2D = $TreeTrunk/TreeLeave
 
 var log_scene = preload("res://scenes/environment/small_log.tscn")
 
@@ -13,11 +11,6 @@ func _ready() -> void:
 	
 func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
-	tree_trunk.material.set_shader_parameter("shake_intensity", 0.5)
-	tree_leave.material.set_shader_parameter("shake_intensity", 0.5)
-	await get_tree().create_timer(1.0).timeout
-	tree_trunk.material.set_shader_parameter("shake_intensity", 0.0)
-	tree_leave.material.set_shader_parameter("shake_intensity", 0.0)
 
 func on_max_damage_reached() -> void:
 	call_deferred("add_log_scene")
--- scenes/environment/mid_tree.gd
+++ scenes/environment/mid_tree.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
 @onready var tree_trunk: Sprite2D = $TreeTrunk
-@onready var tree_leave: Sprite2D = $TreeTrunk/TreeLeave
+@onready var tree_leave: Sprite2D = $TreeTrunk/TreeLeaves
 
 var log_scene = preload("res://scenes/environment/small_log.tscn")
 
@@ -18,7 +18,7 @@ func on_hurt(hit_damage: int) -> void:
 	await get_tree().create_timer(1.0).timeout
 	tree_trunk.material.set_shader_parameter("shake_intensity", 0.0)
 	tree_leave.material.set_shader_parameter("shake_intensity", 0.0)
-
+	
 func on_max_damage_reached() -> void:
 	call_deferred("add_log_scene")
 	
